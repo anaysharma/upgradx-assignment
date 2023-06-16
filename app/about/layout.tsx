9f40631da639af534119c4b898e3d07039b91e13
Anay Sharma <anaysharma8791@gmail.com>
2023-06-16 18:19:30 +0530

    initial commit

---
+++ app/about/layout.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+	title: 'ZET - About us',
+	description: '',
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+	return <>{children}</>;
+}
